fe79b94e6a75a13546cde493406044c2d0d4a421
Apac <jeff@templarit.com.au>
2017-07-20 16:06:44 +1000

    Flesh out grapher

--- DelaunayGrapher.cs
+++ DelaunayGrapher.cs
@@ -1,16 +1,61 @@
-﻿using System.Collections;
+﻿using Delaunay.Geo;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DelaunayGrapher : MonoBehaviour {
+public class DelaunayGrapher {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Delaunay.Voronoi voroni;
+
+    public DelaunayGrapher(List<Vector2> centerPoints)
+    {
+        List<uint> colors = new List<uint>();
+        for (int i = 0; i < centerPoints.Count; i++)
+        {
+            colors.Add(0);
+        }
+
+        voroni = new Delaunay.Voronoi(centerPoints, colors, new Rect(0, 0, 200, 200));
+    }
+
+    /// <summary>
+    /// Find minium amount of connecting line segments between supplied room center points.
+    /// </summary>
+    /// <param name="percentOfSegementsAboveMinThreshold">Percent of extra connecting line segments to return. If 0% then only returns min spanning tree.</param>
+    /// <returns></returns>
+    public List<LineSegment> FindLineSegments(float percentOfSegementsAboveMinThreshold)
+    {
+        List<LineSegment> dTriangulation = voroni.DelaunayTriangulation();
+
+        List<LineSegment> tree = voroni.SpanningTree(Delaunay.KruskalType.MINIMUM);
+
+        int numOfExtraSegments = Mathf.RoundToInt((float)dTriangulation.Count * percentOfSegementsAboveMinThreshold);
+
+        List<LineSegment> finalSegments = new List<LineSegment>();
+
+        int extraSegCount = 0;
+
+        foreach (LineSegment segment in dTriangulation)
+        {
+            bool foundInTree = false;
+
+            // always add the min spanning tree segments to final.
+            foreach (LineSegment treeSeg in tree) {
+                if (segment.p0.Value == treeSeg.p0.Value &&
+                    segment.p1.Value == treeSeg.p1.Value) {
+                    finalSegments.Add(segment);
+                    foundInTree = true;
+                    break;
+                }
+            }
+
+            // Add in the extra segements.
+            if (!foundInTree && extraSegCount < numOfExtraSegments) {
+                finalSegments.Add(segment);
+                extraSegCount++;
+            }
+        }
+
+        return finalSegments;
+    }
 }
