b17206258ed97eecbe5458b13722a556183f8ef3
Apac <jeff@templarit.com.au>
2017-07-20 16:12:42 +1000

    Change to static

--- DelaunayGrapher.cs
+++ DelaunayGrapher.cs
@@ -2,32 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class DelaunayGrapher {
-
-    private Delaunay.Voronoi voroni;
-
-    public DelaunayGrapher(List<Vector2> centerPoints)
-    {
-        List<uint> colors = new List<uint>();
-        for (int i = 0; i < centerPoints.Count; i++)
-        {
-            colors.Add(0);
-        }
-
-        voroni = new Delaunay.Voronoi(centerPoints, colors, new Rect(0, 0, 200, 200));
-    }
+public static class DelaunayGrapher {
 
     /// <summary>
     /// Find minium amount of connecting line segments between supplied room center points.
     /// </summary>
     /// <param name="percentOfSegementsAboveMinThreshold">Percent of extra connecting line segments to return. If 0% then only returns min spanning tree.</param>
     /// <returns></returns>
-    public List<LineSegment> FindLineSegments(float percentOfSegementsAboveMinThreshold)
+    public static List<LineSegment> FindLineSegments(List<Vector2> centerPoints, float percentOfSegementsAboveMinThreshold)
     {
-        List<LineSegment> dTriangulation = voroni.DelaunayTriangulation();
+        Delaunay.Voronoi v = CreateGraph(centerPoints);
 
-        List<LineSegment> tree = voroni.SpanningTree(Delaunay.KruskalType.MINIMUM);
+        List<LineSegment> dTriangulation = v.DelaunayTriangulation();
+
+        List<LineSegment> tree = v.SpanningTree(Delaunay.KruskalType.MINIMUM);
 
         int numOfExtraSegments = Mathf.RoundToInt((float)dTriangulation.Count * percentOfSegementsAboveMinThreshold);
 
@@ -57,5 +47,16 @@ public class DelaunayGrapher {
         }
 
         return finalSegments;
+    }
+
+    private static Delaunay.Voronoi CreateGraph(List<Vector2> centerPoints)
+    {
+        List<uint> colors = new List<uint>();
+        for (int i = 0; i < centerPoints.Count; i++)
+        {
+            colors.Add(0);
+        }
+
+        return new Delaunay.Voronoi(centerPoints, colors, new Rect(0, 0, 200, 200));
     }
 }
