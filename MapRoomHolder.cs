1a5abddafde518430ec94e7f51270ccd2745356c
Apac <Jeff@templarit.com.au>
2017-08-04 18:05:56 +1000

    Change holder to Monobehavoir so it can be assigned to gameobject

---
+++ MapRoomHolder.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A component that is placed on a physical room game object. This allows the gameobject to hold its MapRoom State.
+/// </summary>
+public class MapRoomHolder : MonoBehaviour {
+    public MapRoom mapRoom;
+}
--- Scriptable Objects/MapRoomHolder.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "Helpers/MapRoomHolder", order = 1)]
-public class MapRoomHolder : ScriptableObject {
-    public MapRoom mapRoom;
-}
