fb821bccc48ee677d296aaecc7db3a4f2aae21bd
Apac <Jeff@templarit.com.au>
2017-08-04 16:15:12 +1000

    Add ctor and properties

--- Scriptable Objects/MapData.cs
+++ Scriptable Objects/MapData.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapData : MonoBehaviour {
+public class MapData : ScriptableObject
+{
+    public List<MapRoom> fillerRooms { get; private set; }
+    public List<Line> hallwayLines { get; private set; }
+    public List<MapRoom> hallwayRooms { get; private set; }
+    public List<MapRoom> hubRooms { get; private set; }
+    public int[][] map { get; private set; }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public MapData(int[][] map, List<MapRoom> hubRooms, List<MapRoom> hallwayRooms, List<MapRoom> fillerRooms, List<Line> hallwayLines)
+    {
+        this.map = map;
+        this.hubRooms = hubRooms;
+        this.hallwayRooms = hallwayRooms;
+        this.fillerRooms = fillerRooms;
+        this.hallwayLines = hallwayLines;
+    }
 }
