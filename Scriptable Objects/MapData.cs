948dc91066a2a6e1226970322626b0ca95ae3265
Apac <Jeff@templarit.com.au>
2017-08-07 13:48:13 +1000

    Change map array to RoomType

--- Scriptable Objects/MapData.cs
+++ Scriptable Objects/MapData.cs
@@ -9,9 +9,9 @@ public class MapData
     public List<MapRoom> hallwayRooms { get; private set; }
     public List<MapRoom> hubRooms { get; private set; }
     public Point greatestPoint { get; private set; }
-    public int[][] map { get; private set; }
+    public RoomType[][] map { get; private set; }
 
-    public MapData(int[][] map, List<MapRoom> hubRooms, List<MapRoom> hallwayRooms, List<MapRoom> fillerRooms, List<Line> hallwayLines, Point greatestPoint)
+    public MapData(RoomType[][] map, List<MapRoom> hubRooms, List<MapRoom> hallwayRooms, List<MapRoom> fillerRooms, List<Line> hallwayLines, Point greatestPoint)
     {
         this.map = map;
         this.hubRooms = hubRooms;
