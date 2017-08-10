c170a5e313d82ebd88c3763021504bfe1121eb94
Apac <Jeff@templarit.com.au>
2017-08-10 18:32:03 +1000

    Allow points to be nullable

--- Line.cs
+++ Line.cs
@@ -1,9 +1,9 @@
 ﻿
 public class Line {
-    public UnityEngine.Vector2 p0 { get; private set; }
-    public UnityEngine.Vector2 p1 { get; private set; }
+    public UnityEngine.Vector2? p0 { get; private set; }
+    public UnityEngine.Vector2? p1 { get; private set; }
 
-    public Line(UnityEngine.Vector2 p0, UnityEngine.Vector2 p1) {
+    public Line(UnityEngine.Vector2? p0, UnityEngine.Vector2? p1) {
         this.p0 = p0;
         this.p1 = p1;
     }
