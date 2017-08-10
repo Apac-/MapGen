b2b884fe134c52925acacfa4986c20cf77bae603
Apac <Jeff@templarit.com.au>
2017-08-10 18:16:50 +1000

    Clean up

--- IMapRoomTools.cs
+++ IMapRoomTools.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public interface IMapRoomTools
 {
     List<MapRoom> CreateRoomsFromFiller(RoomType[][] map, IMapRoomFactory roomFactory);
+
     List<MapRoom> FindHallwayRooms(List<Line> hallwayLines, List<MapRoom> hubRooms);
+
     List<MapRoom> FindHubRooms(List<MapRoom> rooms, float cutoff);
+
     Point FindLowestPointInRooms(List<MapRoom> rooms);
+
     MapRoom FindRoomContainingPoint(List<MapRoom> rooms, Vector2 point);
+
     bool IsPointBetweenXBoundariesOfGivenRooms(Point midPoint, MapRoom r0, MapRoom r1, int buffer);
+
     bool IsPointBetweenYBoundariesOfGivenRooms(Point midPoint, MapRoom r0, MapRoom r1, int buffer);
+
     Point MidPointBetweenMapRooms(MapRoom r0, MapRoom r1);
 }
--- MapRoomTools.cs
+++ MapRoomTools.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class MapRoomTools : IMapRoomTools
 {
-
     public Point MidPointBetweenMapRooms(MapRoom r0, MapRoom r1)
     {
         Point r0c = r0.centerPoint;
