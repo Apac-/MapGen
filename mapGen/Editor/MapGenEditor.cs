2d1f72e3c1d99cf5a96eb38aa543d72dbc50e86c
Apac <Jeff@templarit.com.au>
2017-08-14 16:50:29 +1000

    Add editor button to generate map

---
+++ mapGen/Editor/MapGenEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGen))]
+public class MapGenEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MapGen mapGen = (MapGen)target;
+
+        if (GUILayout.Button("Generate Map"))
+            mapGen.Generate();
+    }
+}
